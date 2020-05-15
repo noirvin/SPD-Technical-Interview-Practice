610d761ee508626e619c1995f1d478e08adbe740
noirvin <vin.sp97@googlemail.com>
2020-05-15 15:38:38 -0700

    last leet code problem

--- homework8.py
+++ homework8.py
@@ -11,8 +11,41 @@ def binary_sum(a,b):
     str_prepared = str(bin(sum)).replace('0b','')
 
     return str_prepared
+
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+
+Complexity: O(n^2) since there is a nested while loop
+'''
+
+def two_sum(nums, target):
+    indices=[]
+    i=0
+    j=i
+
+    while i<= len(nums)-1:
+
+        while j<=len(nums)-1:
+
+            if nums[i]+nums[j]==target:
+                indices.append(i)
+                indices.append(j)
+                return indices
+            else:
+                j+=1
+        i+=1
+    return "valid indices not found"
+
+
 test = ['11','1']
 test2= ['1010','10111']
 
 print(binary_sum(test[0],test[1]))
 print(binary_sum(test2[0],test2[1]))
+
+two_sum_list = [2,7,11,15]
+
+print(two_sum(two_sum_list,9))
