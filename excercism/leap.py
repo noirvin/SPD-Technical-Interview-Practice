d7b4aa54f164c42774ebf79e68c03671ed8a8d01
noirvin <vin.sp97@googlemail.com>
2020-05-15 01:02:01 -0700

    excercism

---
+++ excercism/leap.py
@@ -0,0 +1,11 @@
+def leap_check(year):
+    # check if year is divisible by 4 and either indivisible by 100 or divisible by 400,
+    # then it's leap year
+    if year%4==0 and (year%100!=0 or year%400==0):
+
+        return True
+    # when it's a common year
+    else:
+        return False
+
+print(leap_check(1987),leap_check(-1987),leap_check(2000),leap_check(1200))
